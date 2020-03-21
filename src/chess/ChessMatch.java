cc08d1a22c205427dd589ada3c216b093a6a311f
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-03-21 14:11:01 -0300

    placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -20,16 +20,16 @@ public class ChessMatch {
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i,j);
-			}
-			
+			}	
 		}
 	  return mat;	
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+	 board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+	 board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+	 board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
+	 
 	}
 	
 }
